e5e2e50b91b562a2ddf3f788bfedf2bd705da3c4
John Rooey <JROOEY@mail.depaul.edu>
2020-01-28 19:21:10 -0600

    finished

--- Hw1Project/Hw1Project.py
+++ Hw1Project/Hw1Project.py
@@ -1,6 +1,5 @@
 #John Rooney 1/14/20 Homework 1 I have not given or received any unauthorized assistance on this assignment
 
-#Deleted this comment
 def grade():
     """Predicts and prints predicted grade in DSC 430."""
     first = input('First name: ')
@@ -9,11 +8,11 @@ def grade():
     if iq > 130:
         print('You are a Liar')
     elif 110 < iq <= 130:
-        print('{} {} will probably get a B'.format(first, last))
+        print('{} {} will likely get a B'.format(first, last))
     elif 90 <= iq <= 110:
-        print('{} {} will probably get a C'.format(first, last))
+        print('{} {} will likely get a C'.format(first, last))
     else:
-        print('{} {} will probably fail'.format(first, last))
+        print('{} {} will likely get a D'.format(first, last))
     return
 
 def func(lst):
@@ -51,7 +50,7 @@ def func4(lst):
     else:
         return -1
 
-def func5(lst1, lst2):
+def overlap(lst1, lst2):
     """Returns overlap area of 2 squares."""
     if func2(lst1) == False or func2(lst2) == False:
         return -1
@@ -65,48 +64,35 @@ def func5(lst1, lst2):
     # Overlapping area of x-values
     if sq1[0] <= sq2[0]:
         if sq1[1] <= sq2[0]:
-            print('no X overlap')
             return -1
         elif sq2[0] < sq1[1] <= sq2[1]:
             overlapx = sq1[1] - sq2[0]
-            print('X overlap is ' + str(overlapx))
         else:
             overlapx = lst2[2]
-            print(overlapx)
     else:
         if sq2[1] <= sq1[0]:
-            print('no X overlap 2')
             return -1
         elif sq1[0] < sq2[1] <= sq1[1]:
             overlapx = sq2[1] - sq1[0]
-            print('22 X overlap is ' + str(overlapx))
         else:
             overlapx = lst1[2]
-            print(overlapx, '2')
 
     # Overlapping area of Y-values
-    if sq1[2] <= sq2[3]:
+    if sq1[2] <= sq2[2]:
         if sq1[3] <= sq2[2]:
-            print('no Y overlap')
             return -1
         elif sq2[2] < sq1[3] <= sq2[3]:
             overlapy = sq1[3] - sq2[2]
-            print('Y overlap is ' + str(overlapy))
         else:
             overlapy = lst2[2]
-            print(overlapy, 'yy')
     else:
-        if sq2[1] <= sq1[2]:
-            print('no Y overlap 2')
+        if sq2[3] <= sq1[2]:
             return -1
         elif sq1[2] < sq2[3] <= sq1[2]:
             overlapy = sq2[3] - sq1[2]
-            print('22 Y overlap is ' + str(overlapy))
         else:
             overlapy = lst1[2]
-            print(overlapy, 'y')
-
-func5([1,3,2],[1,1,2])
+    return overlapx * overlapy
 
 def prime(x):
     if type(x) != int:
